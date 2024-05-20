8c44c93e2bf59d8350ad6edd7b17558a411a6c1e
Alexander Pridava <sanyapridava@mail.ru>
2024-05-21 00:21:22 +0300

    Isolation serialize struct

--- Models/Mocks/RequestsToDB.go
+++ Models/Mocks/RequestsToDB.go
@@ -1,5 +1,9 @@
 package Mocks
 
+import "EventsList/Models/Mocks/serialize_struct"
+
+type Event = serialize_struct.Event
+
 func (database *DB) GetCatigories() map[string][]string {
 
 	list_catigories := map[string][]string{
@@ -9,24 +13,16 @@ func (database *DB) GetCatigories() map[string][]string {
 	return list_catigories
 }
 
-func (database *DB) GetEvents(catigory string) any {
-
-	type Event struct {
-		id_creator string
-		catigory   string
-		date       string
-		address    string
-		definition string
-	}
+func (database *DB) GetEvents(catigory string) []Event {
 
 	list_events := []Event{
-		Event{id_creator: "727473E7-14B3-4580-A086-2199430834DB", catigory: catigory, date: "25.05.2024 10:21",
-			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
-		Event{id_creator: "717473E7-54B3-4581-B086-2199430834DA", catigory: catigory, date: "02.06.2024 11:35",
-			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
+		Event{Id_creator: "727473E7-14B3-4580-A086-2199430834DB", Catigory: catigory, Date: "25.05.2024 10:21",
+			Address: "г. Краснодар, ул. Ставропольская 149", Definition: "<Информация о мероприятии>"},
+		Event{Id_creator: "717473E7-54B3-4581-B086-2199430834DA", Catigory: catigory, Date: "02.06.2024 11:35",
+			Address: "г. Краснодар, ул. Ставропольская 149", Definition: "<Информация о мероприятии>"},
 
-		Event{id_creator: "717473E7-54B3-4580-A086-2199430834DA", catigory: catigory, date: "02.06.2024 11:35",
-			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
+		Event{Id_creator: "717473E7-54B3-4580-A086-2199430834DA", Catigory: catigory, Date: "02.06.2024 11:35",
+			Address: "г. Краснодар, ул. Ставропольская 149", Definition: "<Информация о мероприятии>"},
 	}
 
 	return list_events
