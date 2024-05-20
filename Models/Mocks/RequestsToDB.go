cb4a83b813c1bbcf3aa24532c5252e837db365ef
Alexander Pridava <sanyapridava@mail.ru>
2024-05-20 23:00:58 +0300

    Create handler what have catigory  give list of events

--- Models/Mocks/RequestsToDB.go
+++ Models/Mocks/RequestsToDB.go
@@ -8,3 +8,26 @@ func (database *DB) GetCatigories() map[string][]string {
 
 	return list_catigories
 }
+
+func (database *DB) GetEvents(category string) any {
+
+	type Event struct {
+		id_creator string
+		catigory   string
+		date       string
+		address    string
+		definition string
+	}
+
+	list_events := []Event{
+		Event{id_creator: "727473E7-14B3-4580-A086-2199430834DB", catigory: "РСО", date: "25.05.2024 10:21",
+			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
+		Event{id_creator: "717473E7-54B3-4581-B086-2199430834DA", catigory: "СНО ФКТиПМ", date: "02.06.2024 11:35",
+			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
+
+		Event{id_creator: "717473E7-54B3-4580-A086-2199430834DA", catigory: "СНО ФКТиПМ", date: "02.06.2024 11:35",
+			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
+	}
+
+	return list_events
+}
