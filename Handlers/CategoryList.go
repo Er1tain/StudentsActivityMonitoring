1266ee8289626962ab701728300c8566e970b4dd
Alexander Pridava <sanyapridava@mail.ru>
2024-05-21 00:29:19 +0300

    Handling only get-request

--- Handlers/CategoryList.go
+++ Handlers/CategoryList.go
@@ -1,7 +1,25 @@
 package handlers
 
-import "net/http"
+import (
+	"EventsList/Models/Mocks"
+	"encoding/json"
+	"net/http"
+)
 
 func GetListCategories(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		database := Mocks.ConnectToDB()
+		list_catigories := database.GetCatigories()
 
+		data, err := json.Marshal(list_catigories)
+		if err != nil {
+			panic("Fatal error!")
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+		return
+	}
+
+	data, _ := json.Marshal("Only GET!")
+	w.Write(data)
 }
--- Handlers/EventsList.go
+++ Handlers/EventsList.go
@@ -1,7 +1,27 @@
 package handlers
 
-import "net/http"
+import (
+	"EventsList/Models/Mocks"
+	"encoding/json"
+	"net/http"
+)
 
 func GetListEvents(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		database := Mocks.ConnectToDB()
+		catigory := r.URL.Query().Get("catigory")
+		list_events := database.GetEvents(catigory)
+
+		data, err := json.Marshal(list_events)
+		if err != nil {
+			panic("Fatal error!")
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+		return
+	}
+
+	data, _ := json.Marshal("Only GET!")
+	w.Write(data)
 
 }
