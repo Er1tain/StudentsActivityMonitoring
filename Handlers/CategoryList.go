e72d71815477c30f0dfaf63365940fd325e8fe0c
Alexander Pridava <sanyapridava@mail.ru>
2024-05-20 20:11:38 +0300

    Create handler for all requests

---
+++ Handlers/CategoryList.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "net/http"
+
+func GetListCategories(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ Handlers/DeleteEvent.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "net/http"
+
+func DeleteEvent(w http.ResponseWriter, r *http.Request) {
+
+}
