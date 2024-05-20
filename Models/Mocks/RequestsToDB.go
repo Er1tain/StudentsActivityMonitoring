fed79d47ddd0d6d62fea0cd67da55a3d2de4a205
Alexander Pridava <sanyapridava@mail.ru>
2024-05-20 23:03:58 +0300

    Event give arguments from function

--- Models/Mocks/RequestsToDB.go
+++ Models/Mocks/RequestsToDB.go
@@ -9,7 +9,7 @@ func (database *DB) GetCatigories() map[string][]string {
 	return list_catigories
 }
 
-func (database *DB) GetEvents(category string) any {
+func (database *DB) GetEvents(catigory string) any {
 
 	type Event struct {
 		id_creator string
@@ -20,12 +20,12 @@ func (database *DB) GetEvents(category string) any {
 	}
 
 	list_events := []Event{
-		Event{id_creator: "727473E7-14B3-4580-A086-2199430834DB", catigory: "РСО", date: "25.05.2024 10:21",
+		Event{id_creator: "727473E7-14B3-4580-A086-2199430834DB", catigory: catigory, date: "25.05.2024 10:21",
 			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
-		Event{id_creator: "717473E7-54B3-4581-B086-2199430834DA", catigory: "СНО ФКТиПМ", date: "02.06.2024 11:35",
+		Event{id_creator: "717473E7-54B3-4581-B086-2199430834DA", catigory: catigory, date: "02.06.2024 11:35",
 			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
 
-		Event{id_creator: "717473E7-54B3-4580-A086-2199430834DA", catigory: "СНО ФКТиПМ", date: "02.06.2024 11:35",
+		Event{id_creator: "717473E7-54B3-4580-A086-2199430834DA", catigory: catigory, date: "02.06.2024 11:35",
 			address: "г. Краснодар, ул. Ставропольская 149", definition: "<Информация о мероприятии>"},
 	}
 
